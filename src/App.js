41d2ccb91c4b54750d8c5c2044d86b1fc1a86707
Anirudh <anirudh.venkatesh@gmail.com>
2019-11-05 22:58:42 -0800

    Smart brain facial recognition - initial commit (includes front page, no api)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Particles from 'react-particles-js';
+import Navigation from './components/Navigation/Navigation.js'
+import ImageUrl from './components/ImageUrl/ImageUrl.js'
+import Rank from './components/Rank/Rank.js'
+import "tachyons"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const particlesOptions = {
+  particles: {
+    number:{
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: false,
+        mode: "repulse"
+      },
+      onclick: {
+        enable: true,
+        mode: "push"
+      },
+      resize: true
+    }
+  }
+};
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: "",
+    }
+  }
+
+  onInputChange = (event) =>{
+    console.log(event);
+  }
+
+  onDetect = () => {
+    console.log("click");
+    // fetch("http://localhost:5000/test")
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Particles className='particles' params={particlesOptions} />
+        <Navigation/>
+        <Rank/>
+        <ImageUrl onInputChange={this.onInputChange} onDetect={this.onDetect}/>
+        {/* <FaceRecognition/> */}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = () => {
+    return(
+        <div>
+            <p className='pointer tr pa3 underline br2 shadow-5'>Sign Out!</p>
+        </div>
+    )
+}
+
+export default Navigation;
