83800fa261b5251fabf2af376c2d197118795137
Anirudh <anirudh.venkatesh@gmail.com>
2019-11-17 23:38:02 -0800

    fixed minor bug with index

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -6,8 +6,8 @@ const FaceRecognition = ({boxes, imageUrl}) => {
         <div className='center ma'>
             <div id = 'image_with_boxes' className='absolute mt2'>
                 <img id='detected_image' alt='' src = {imageUrl} width='500px' height='auto'></img>
-                {boxes.map(box => {
-                  return(<div className='bounding-box' style={{top: box.top_row, right: box.right_col, bottom: box.bottom_row, left: box.left_col}}></div>)  
+                {boxes.map((box,index) => {
+                  return(<div className='bounding-box' key={index} style={{top: box.top_row, right: box.right_col, bottom: box.bottom_row, left: box.left_col}}></div>)  
                 })}
             </div>
         </div>
