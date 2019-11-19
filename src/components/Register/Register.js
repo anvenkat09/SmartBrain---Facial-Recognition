852b53dad90606c6e186f14bd006162c0b791927
Anirudh <anirudh.venkatesh@gmail.com>
2019-11-18 23:45:02 -0800

    testing vars

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component{
     }
 
     onSubmitSignin = () => {
-        console.log(process.env.REGISTER);
+        console.log(process.env.REACT_APP_REGISTER);
         fetch(process.env.REGISTER, {
             method: 'post',
             headers: {
